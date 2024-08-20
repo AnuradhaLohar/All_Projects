c02e1a9f5e63a8ba7eb28b3d45e0e40dfb113ba4
AnuradhaLohar <loharanuradha644@gmail.com>
2024-08-20 16:36:53 +0530

    21 project done

---
+++ 21_Incrementing_counter/pro21.js
@@ -0,0 +1,24 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+
+    const updatecounter = () => {
+        const target = +counter.getAttribute('data-target')
+
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updatecounter ,1)
+        } else {
+            counter.innerHTML = target
+        }
+}
+
+updatecounter()
+
+})
