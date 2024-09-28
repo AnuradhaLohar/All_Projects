3eaa5b0a0d82509946b3c0cec9e6cfb20b47dd50
AnuradhaLohar <loharanuradha644@gmail.com>
2024-09-28 15:52:58 +0530

    Feedback UI Design  project 49 done

---
+++ 49_Feedback_UI_Design/pro49.js
@@ -0,0 +1,31 @@
+const ratings = document.querySelectorAll('.rating')
+const submitBtn = document.querySelector('.btn')
+const panel =document.querySelector('#panel')
+let selectedRating = ''
+
+ratings.forEach(rating => {
+    rating.addEventListener('click', () => {
+        for (let i = 0; i < ratings.length; i++) {
+             ratings[i].classList.remove('active')  
+        }
+        rating.classList.add('active')
+
+        const smallContent = rating.querySelector('.rating small')
+         selectedRating = smallContent.textContent
+
+    })
+})
+
+submitBtn.addEventListener('click' , () => {
+    console.log(selectedRating);
+
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback : ${selectedRating}</strong>
+        <p style="font-size:18px; font-family:'Montserrat', sans-serif; ">We'll use your feedback to improve our customer support</p>
+    `
+    
+})
+
